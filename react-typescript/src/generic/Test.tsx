05d5c824ed7a1131afb4bf247499da00d45dd5dd
pc <meekailkhan123aar@gmail.com>
2025-02-26 01:04:26 +0530

    custom component

--- react-typescript/src/generic/Test.tsx
+++ react-typescript/src/generic/Test.tsx
@@ -2,7 +2,7 @@
 type TestOwnProps<E extends React.ElementType>  = {
     children : React.ReactNode
     size? : 'sm' | 'md' | 'xl'
-    color? : 'primary' | 'secondry'
+    color? : 'primary' | 'secondry' |'random'
     as? : E
 
 }
